b0790d49a3c36cd622de7402b666e4554678d121
zgeniuscoders <zgeniuscoders.com>
2025-02-10 14:15:22 +0100

    enregistrement des attributs dans poste et departement

--- app/models/department.ts
+++ app/models/department.ts
@@ -1,13 +1,21 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column, hasMany } from '@adonisjs/lucid/orm'
+import * as relations from '@adonisjs/lucid/types/relations'
+import Poste from '#models/poste'
 
 export default class Department extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare name: string
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @hasMany(() => Poste)
+  declare personnel: relations.HasMany<typeof Poste>
 }
--- app/models/poste.ts
+++ app/models/poste.ts
@@ -1,9 +1,20 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import Department from '#models/department'
+import * as relations from '@adonisjs/lucid/types/relations'
 
 export default class Poste extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare grade: string
+
+  @column()
+  declare departmentId: number
+
+  @belongsTo(() => Department)
+  declare department: relations.BelongsTo<typeof Department>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
