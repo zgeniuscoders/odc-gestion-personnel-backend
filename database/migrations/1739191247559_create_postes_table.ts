cf91c12465a09b994d203c6bd8901752f648ca19
zgeniuscoders <zgeniuscoders.com>
2025-02-10 14:00:42 +0100

    correction faute orthographe dans la  migration de grade

--- database/migrations/1739191247559_create_postes_table.ts
+++ database/migrations/1739191247559_create_postes_table.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
 
-      table.string("grande").notNullable()
+      table.string("grade").notNullable()
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
