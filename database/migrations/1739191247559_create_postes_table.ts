82373309bb97b89f324416aeba4660bfc6d271d0
zgeniuscoders <zgeniuscoders.com>
2025-02-10 13:59:47 +0100

    correction faute orthographe dans les migrations

--- database/migrations/1739191247559_create_postes_table.ts
+++ database/migrations/1739191247559_create_postes_table.ts
@@ -6,7 +6,9 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+
       table.string("grade").notNullable()
+
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
